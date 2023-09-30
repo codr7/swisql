12d8ce2ab8effab9a983ee60a2378fe4c4534256
codr7 <codr7@protonmail.com>
2023-09-30 21:06:09 +0200

    Add libs.

--- Package.swift
+++ Package.swift
@@ -4,20 +4,31 @@ import PackageDescription
 
 let package = Package(
   name: "Swisql",
+
   products: [
     .library(
-      name: "Swisql",
+      name: "SwisqlStatic",
+      type: .static,
       targets: ["Swisql"]),
+
+    .library(
+      name: "SwisqlDynamic",
+      type: .dynamic,
+      targets: ["Swisql"]),
+
     .executable(
       name: "SwisqlTests",
       targets: ["SwisqlTests"])
   ],
+
   dependencies: [
   ],
+  
   targets: [
     .target(
       name: "Swisql",
       dependencies: []),
+    
     .executableTarget(
       name: "SwisqlTests",
       dependencies: ["Swisql"]),
